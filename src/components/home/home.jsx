ce96cf427c41071095a6d0c919a36424a82026f2
saloni-kapoor <salonikapoor742@gmail.com>
2026-09-19 04:55:51 -0700

    Update image source paths in home component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
                     <img
                         className="sm:w-96 w-48"
-                        src="/images/image.png"
+                        src="src/images/image.png"
                         alt="image2"
                     />
                 </div>
@@ -43,7 +43,7 @@ export default function Home() {
             <div className="grid  place-items-center sm:mt-20">
                 <img
                     className="sm:w-96 w-48"
-                    src="/images/image1.png"
+                    src="src/images/image1.png"
                     alt="image2"
                 />
             </div>
