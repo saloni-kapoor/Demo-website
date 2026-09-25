1fff134a83865e083ebd1eded507a97854204ec6
saloni-kapoor <salonikapoor742@gmail.com>
2026-09-25 07:24:06 -0700

    Refactor Home component layout and styling

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,17 +4,12 @@ import { Link } from 'react-router-dom'
 export default function Home() {
     return (
         <div className="mx-auto w-full max-w-7xl">
-            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 py-10 sm:py-16">
-                <div className="relative z-10 max-w-screen-xl px-4 pb-10 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
-
-                    <div className="max-w-xl space-y-8 text-center sm:text-right sm:ml-auto">
-
+            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+                <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                    <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                         <h2 className="text-4xl font-bold sm:text-5xl">
                             Download Now
-
-                            <span className="hidden sm:block text-4xl">
-                                Lorem Ipsum
-                            </span>
+                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
                         </h2>
 
                         <Link
@@ -31,31 +26,30 @@ export default function Home() {
                             >
                                 <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
                             </svg>
-
                             &nbsp; Download now
                         </Link>
-
                     </div>
                 </div>
-                <div className="absolute left-0 top-0 flex items-center w-full h-full pointer-events-none">
+
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
                     <img
-                        className="relative z-0 w-48 sm:w-72 md:w-80 lg:w-96"
+                        className="sm:w-96 w-48"
                         src={`${import.meta.env.BASE_URL}images/image.png`}
                         alt="image2"
                     />
                 </div>
             </aside>
-            <div className="grid place-items-center mt-10 sm:mt-20">
+
+            <div className="grid  place-items-center sm:mt-20">
                 <img
-                    className="w-48 sm:w-72 md:w-80 lg:w-96"
+                    className="sm:w-96 w-48"
                     src={`${import.meta.env.BASE_URL}images/image1.png`}
                     alt="image2"
                 />
             </div>
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
-                Lorem Ipsum Yojo
-            </h1>
 
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
-    )
+    );
 }
+
